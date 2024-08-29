fbdf99422ed44967d017289374b84c4d9bb99130
lihon <gelf.learing@gmail.com>
2024-08-29 21:37:08 +0300

    addign pics

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ const Homepage = () => {
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-          <Image src="/hero.png" alt="" fill className="object-contain" />
+          <Image src="/hero.jpeg" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
