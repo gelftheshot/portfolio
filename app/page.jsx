c34fbd7b207caf11eec0c52e35a2945386e3fda1
lihon <gelf.learing@gmail.com>
2024-08-29 17:59:30 +0300

    adding info about my code

---
+++ app/page.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Homepage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image src="/hero.png" alt="" fill className="object-contain" />
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Innovating with Code, AI, and DevOps.
+          </h1>
+          <p className="md:text-xl">
+            Welcome to my digital portfolio. I'm Lihon Gebre, a Software Engineer, 
+            AI Developer, and DevOps Engineer. My work combines cutting-edge 
+            technology with practical solutions, showcasing a commitment to 
+            excellence across multiple domains of modern computing.
+          </p>
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Homepage;
