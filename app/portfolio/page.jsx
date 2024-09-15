f70d8cfd8c1791d039e5f90b99648a3dc2a7ab3d
lihon <gelf.learing@gmail.com>
2024-09-15 08:51:54 +0300

    fixing a little bit bug

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -136,33 +136,36 @@ const PortfolioPage = () => {
           </motion.div>
         </div>
       </div>
-      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
-        <h1 className="text-8xl">Do you have a project?</h1>
-        <div className="relative">
-          <motion.svg
-            animate={{ rotate: 360 }}
-            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
-            viewBox="0 0 300 300"
-            className="w-64 h-64 md:w-[500px] md:h-[500px] "
-          >
-            <defs>
-              <path
-                id="circlePath"
-                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
-              />
-            </defs>
-            <text fill="#000">
-              <textPath xlinkHref="#circlePath" className="text-xl">
-                Software Engineer, AI Developer, and DevOps Engineer
-              </textPath>
-            </text>
-          </motion.svg>
-          <Link
-            href="/contact"
-            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
-          >
-            Hire Me
-          </Link>
+      {/* Add padding to create space between portfolio items and the "Do you have a project?" section */}
+      <div className="py-20">
+        <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+          <h1 className="text-6xl md:text-8xl">Do you have a project?</h1>
+          <div className="relative">
+            <motion.svg
+              animate={{ rotate: 360 }}
+              transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+              viewBox="0 0 300 300"
+              className="w-64 h-64 md:w-[500px] md:h-[500px] "
+            >
+              <defs>
+                <path
+                  id="circlePath"
+                  d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+                />
+              </defs>
+              <text fill="#000">
+                <textPath xlinkHref="#circlePath" className="text-xl">
+                  Software Engineer, AI Developer, and DevOps Engineer
+                </textPath>
+              </text>
+            </motion.svg>
+            <Link
+              href="/contact"
+              className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+            >
+              Hire Me
+            </Link>
+          </div>
         </div>
       </div>
     </motion.div>
