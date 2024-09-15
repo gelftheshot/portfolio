8054b30c5d1fa38393271568017faabb48a53d69
lihon <gelf.learing@gmail.com>
2024-09-15 16:59:58 +0300

    updating to fix deployment

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -195,42 +195,8 @@ const AboutPage = () => {
               animate={isExperienceRefInView ? { x: "0" } : {}}
               className="flex flex-col gap-36" // Increased gap between experiences
             >
-              {/* TechInnovate Inc. */}
-              <div className="flex justify-between h-48">
-                <div className="w-1/3 ">
-                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                    Senior Software Engineer & AI Developer
-                  </div>
-                  <div className="p-3 text-sm italic">
-                    Led development of AI-powered software solutions and implemented robust DevOps practices.
-                  </div>
-                  <div className="p-3 text-red-400 text-sm font-semibold">
-                    2020 - Present
-                  </div>
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    TechInnovate Inc.
-                  </div>
-                </div>
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
-                <div className="w-1/3 "></div>
-              </div>
-
               {/* Axion Engineering */}
               <div className="flex justify-between h-48">
-                <div className="w-1/3 "></div>
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
                 <div className="w-1/3 ">
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
                     Full Stack Developer
@@ -245,10 +211,26 @@ const AboutPage = () => {
                     Axion Engineering
                   </div>
                 </div>
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                <div className="w-1/3 "></div>
               </div>
 
               {/* Headstarter AI Fellow */}
               <div className="flex justify-between h-48">
+                <div className="w-1/3 "></div>
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
                 <div className="w-1/3 ">
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
                     AI Engineer Fellow
@@ -263,26 +245,10 @@ const AboutPage = () => {
                     Headstarter AI Fellowship
                   </div>
                 </div>
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
-                <div className="w-1/3 "></div>
               </div>
 
               {/* ALX Africa */}
               <div className="flex justify-between h-48">
-                <div className="w-1/3 "></div>
-                <div className="w-1/6 flex justify-center">
-                  {/* LINE */}
-                  <div className="w-1 h-full bg-gray-600 rounded relative">
-                    {/* LINE CIRCLE */}
-                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                  </div>
-                </div>
                 <div className="w-1/3 ">
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
                     Backend Developer
@@ -297,6 +263,14 @@ const AboutPage = () => {
                     ALX Africa
                   </div>
                 </div>
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                <div className="w-1/3 "></div>
               </div>
             </motion.div>
           </div>
